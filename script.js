a47c543b39397bec9b51fc222e64eb50fc0306eb
Anatolii <vysarionych@gmail.com>
2019-04-24 22:35:29 +0300

    Fourth commit

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
 'use srtrict';
-let money,
-    time;
-
-money = prompt("Your budget?");
-console.log(money);
-
-time = prompt("Date");
-console.log(time);
-
+let money = +prompt("What\'s your monthly budget?", ''),
+    time = prompt("Enter date YYYY-MM-DD", '');
 let appData = {
     budget: money,
+    expenses:{},
+    optionalExpenses:{},
+    income:[],
     timeData: time,
-    expenses:{
-        obligExp,
-        howMuch
-    },
-    optionalExpenses:{
-
-    },
-    income: null,
-    savings: false
+    savings: false,
 };
 
-var obligExp = prompt("Eneter your obligatory expenses");
-console.log(obligExp);
-var howMuch = prompt("How much?");
-console.log(howMuch);
+/*for(let i = 0; i < 2; i++){
+    let a = prompt("Enter necessary expences for this month", ''),
+        b = prompt("How much?", '');
+    if(typeof(a)==='string' && typeof(a) !=null && typeof(b) !=null && a != '' && b != '' && a.length < 50){
+        console.log("done");
+        appData.expenses[a] = b;
+    } else{
 
-alert("Budget for a day - " + appData.budget/30);
+    }
+};*/
+let i=0;
+/*while(i < 2){
+    let a = prompt("Enter necessary expences for this month", ''),
+        b = prompt("How much?", '');
+    if(typeof(a)==='string' && typeof(a) !=null && typeof(b) !=null && a != '' && b != '' && a.length < 50){
+        console.log("done");
+        appData.expenses[a] = b;
+    }
+    i++;
+}*/
+do{
+    let a = prompt("Enter necessary expences for this month", ''),
+        b = prompt("How much?", '');
+    if(typeof(a)==='string' && typeof(a) !=null && typeof(b) !=null && a != '' && b != '' && a.length < 50){
+        console.log("done");
+        appData.expenses[a] = b;
+    }
+    i++;
+}while(i<2);
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Daily budget - " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100){
+    console.log("Minimum income");
+} else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+    console.log("Mediocre income" );
+} else if(appData.moneyPerDay > 2000){
+    console.log("High income");
+} else {
+    console.log("Error ocured")
+}
